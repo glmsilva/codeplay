09c68dbff91598c79fe0fbe19dd9c9b6576c828a
Guilherme Silva <guilherme.gss@outlook.com.br>
2021-05-15 21:42:49 -0300

    add admin register courses

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,29 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    @course = Course.new 
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to @course
+    else
+      flash[:alert] = 'Preencha todos os campos'
+      render :new
+    end
+  end
+  
+  private
+  
+  def course_params
+    params.require(:course).permit(:name, :description,:code, :price, :enrollment_deadline)
+  end
+end
