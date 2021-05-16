2a20b67d37a71a578850bdd27d99c289e13b6210
Guilherme Silva <guilherme.gss@outlook.com.br>
2021-05-15 23:00:25 -0300

    fix error in model course to pass the test and display errors in view

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-  validates :name, :description, :price, :enrollment_deadline, presence: {
+  validates :name, :price, presence: {
     message: 'não pode ficar em branco' }
   validates :code, presence: {message: 'não pode ficar em branco'}, uniqueness: { message: 'já está em uso'}
 
