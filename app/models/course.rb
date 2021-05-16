d13f01290d6cd89550b59aef68ec457db8ca4ae5
Guilherme Silva <guilherme.gss@outlook.com.br>
2021-05-15 22:38:13 -0300

    add unit test for course model and disable registering blank fields

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,6 @@
 class Course < ApplicationRecord
-end
+  validates :name, :description, :price, :enrollment_deadline, presence: {
+    message: 'não pode ficar em branco' }
+  validates :code, presence: {message: 'não pode ficar em branco'}, uniqueness: { message: 'já está em uso'}
+
+end  
