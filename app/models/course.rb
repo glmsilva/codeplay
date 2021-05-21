4cd0f2c1ee336aadaa408b50962ac42ccba085b0
Guilherme Silva <guilherme.gss@outlook.com.br>
2021-05-20 22:07:23 -0300

    i18n config and instructos view

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,5 @@
 class Course < ApplicationRecord
-  validates :name, :price, presence: {
-    message: 'não pode ficar em branco' }
-  validates :code, presence: {message: 'não pode ficar em branco'}, uniqueness: { message: 'já está em uso'}
+  validates :name, :price, presence: true
+  validates :code, presence: true, uniqueness: true
 
 end  
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,4 @@
 class Instructor < ApplicationRecord
   has_one_attached :profile_picture
+  validates :name, :email, presence: true
 end
