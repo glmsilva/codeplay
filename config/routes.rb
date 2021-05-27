03ac32cb7fa7fad86c96a514e0ec75bbac5a6d32
Guilherme Silva <guilherme.gss@outlook.com.br>
2021-05-26 23:24:47 -0300

    add devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
 
   namespace :admin do
@@ -7,4 +8,6 @@ Rails.application.routes.draw do
       resources :lessons, only: %i[index show new create edit update]
     end
   end
+
+  resources :courses, only: %i[index show]
 end
